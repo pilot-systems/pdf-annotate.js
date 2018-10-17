2a5f09b38ffc121e69fc40e3f35f3a0ce8baa0ce
yleclanche <yoan.leclanche@gmail.com>
2018-10-17 17:55:27 +0200

    Fix rendering the same page more than once from scroll event (from https://github.com/instructure/pdf-annotate.js/pull/57/commits/63dc2316b8251c677c7599edcb4805671ed2f118)

--- web/index.js
+++ web/index.js
@@ -17,13 +17,18 @@ PDFJS.workerSrc = './shared/pdf.worker.js';
 
 // Render stuff
 let NUM_PAGES = 0;
+let renderedPages = {};
 document.getElementById('content-wrapper').addEventListener('scroll', function (e) {
   let visiblePageNum = Math.round(e.target.scrollTop / PAGE_HEIGHT) + 1;
   let visiblePage = document.querySelector(`.page[data-page-number="${visiblePageNum}"][data-loaded="false"]`);
   if (visiblePage) {
-    setTimeout(function () {
-      UI.renderPage(visiblePageNum, RENDER_OPTIONS);
-    });
+    // Prevent invoking UI.renderPage on the same page more than once.
+	if ( !renderedPages[visiblePageNum] ) {
+	  renderedPages[visiblePageNum] = true;
+	  setTimeout(function () {
+		UI.renderPage(visiblePageNum, RENDER_OPTIONS);
+	  });
+	}
   }
 });
 
